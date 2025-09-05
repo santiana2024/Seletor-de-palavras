56e17c97f2e879149ba7409415e6c193971db7ab
santiana2024 <besegato.ana@escola.pr.gov.br>
2025-09-05 08:29:01 -0300

    Add files via upload
    aula do dia 05/09/2025

---
+++ main.js
@@ -0,0 +1,18 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
+
+function mostraPalavraChave(){
+   const texto = document.querySelector('#entrada-de-texto').value;
+   const campoResultado = document.querySelector('#resultado-palavrachave');
+   const palavrasChave = processaTexto(texto);
+
+   campoResultado.textContent = palavrasChave.join(",");
+}
+
+
+function processaTexto(texto) {
+   let palavras = texto.split(/\P{L}+/u);
+   return palavras;
+
+
+}
